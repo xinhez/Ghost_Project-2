347a82cd4d550e70e1d5c2fb3a8861a054da2e7a
Xinhe Zhang <xinhezhang@g.harvard.edu>
2024-02-25 13:18:21 -0500

    Add file organizer.

---
+++ reorg_files.py
@@ -0,0 +1,21 @@
+import os 
+import shutil
+
+root = 'data/raw/MultiRegion_tosort'
+outroot = 'data/raw/MultiRegion'
+dates = sorted(os.listdir(root))
+
+for date in dates:
+    sessions = sorted(os.listdir(f'{root}/{date}'))
+    for session in sessions:
+        mouse = '_'.join(session.split('_')[:2])
+        print('date', date)
+        print('mouse', mouse)
+        src = f'{root}/{date}/{session}'
+        if os.path.isdir(src):
+            tgt = f'{outroot}/{mouse}/{date}/{session}'
+            print('src', repr(src))
+            print('tgt', repr(tgt))
+            print()
+            shutil.move(src, tgt)
+    print()
