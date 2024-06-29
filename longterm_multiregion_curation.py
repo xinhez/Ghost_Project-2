9dec8ebd48d4c4652a2859aa586b853ba8b5bd5e
Xinhe Zhang <xinhezhang@g.harvard.edu>
2024-06-29 11:20:00 -0400

    Add curation for M9_7 and M9_8

---
+++ longterm_multiregion_curation.py
@@ -0,0 +1,92 @@
+# threshold = 4.5
+excluded_segments = {
+    'M9_7': {
+        'region1': {
+            1: [7],
+            3: 'all',
+            7: [4],
+            9: [7],
+            17: 'all',
+            19: 'all',
+            28: 'all',
+            32: 'all',
+            34: 'all',
+            39: 'all'
+        },
+        'region2': {
+            2: [6],
+            5: 'all',
+            7: 'all', 
+            8: 'all', 
+            9: 'all', 
+            11: 'all', 
+            12: 'all', 
+            13: 'all', 
+            19: [7],
+            26: 'all',
+            30: 'all',
+            31: 'all',
+            33: 'all',
+            36: 'all',
+            38: [0],
+            41: [0],
+            43: [0],
+            49: [2],
+            50: 'all',
+            51: 'all',
+            52: 'all',
+        }
+    },
+}
+
+# threshold = 5.5
+# symmetry = 0.95
+excluded_segments = {
+    'M9_7': {
+        'region1': {
+            1: [2],
+            2: 'all',
+            19: 'all',
+            29: 'all',
+            33: 'all',
+            35: 'all',
+            39: 'all',
+        },
+        'region2': {
+            4: 'all',
+            5: 'all',
+            6: 'all',
+            7: 'all',
+            8: 'all',
+            9: 'all',
+            10: 'all',
+            11: 'all',
+            12: 'all',
+            33: [0, 4],
+            37: [5, 7],
+            38: 'all',
+            47: 'all',
+            48: 'all',
+            49: 'all',
+            50: 'all',
+        }
+    },
+    'M9_8': {
+        'region1': {
+            1: 'all',
+            3: 'all',
+            15: 'all',
+            17: 'all',
+            19: 'all',
+            20: 'all',
+        },
+        'region2': {
+            11: 'all',
+            12: 'all',
+            18: 'all',
+            22: 'all',
+            30: 'all',
+            36: 'all',
+        }
+    },
+}
